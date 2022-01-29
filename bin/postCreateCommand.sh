e149c189fd16d59b08a9c4e919def34e813b91bc
Tom Waterman <tjwaterman99@gmail.com>
2022-01-29 16:25:06 +0000

    Test why env vars aren't coming thru

--- bin/postCreateCommand.sh
+++ bin/postCreateCommand.sh
@@ -6,11 +6,13 @@
 apt-get update
 apt-get install -y postgresql
 sudo service postgresql start
-sudo -u postgres psql -c "create user artefacts with password 'password'"
-sudo -u postgres createdb artefacts -O artefacts
+sudo -u postgres psql -c "create user $PGUSER with password '$PGPASSWORD'"
+sudo -u postgres createdb $PGDATABASE -O $PGUSER
 
 pipx install poetry
 poetry install
 git submodule update --init --recursive
 
+cmd="poetry run dbt build --project-dir $DBT_PROJECT_DIR"
+echo cmd
 poetry run dbt build --project-dir $DBT_PROJECT_DIR
