5033a734f773b0e9579a11a74d002bd575df1b10
Tom Waterman <tjwaterman99@gmail.com>
2022-02-06 10:54:06 +0100

    Add simple dbt version validation (#49)

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -1,3 +1,5 @@
+import pytest
+
 from artefacts import Manifest
 import artefacts.state
 
@@ -5,3 +7,15 @@ import artefacts.state
 def test_manifest_loads():
     manifest = Manifest.load()
     assert artefacts.state.manifest == manifest
+
+
+def test_manifest_validates_dbt_version(manifest):
+    assert Manifest.validate_metadata(manifest.metadata)
+
+    # Quick way to check that some validation is being ran.
+    outdated_metadata = manifest.metadata.copy(deep=True)
+    outdated_metadata.dbt_version_raw = '0.21'
+    assert outdated_metadata.dbt_version.major == 0
+
+    with pytest.raises(ValueError):
+        Manifest.validate_metadata(outdated_metadata)
