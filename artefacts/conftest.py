660a6e5b2c935a93958f0b256272cd2ccd10d7af
Tom Waterman <tjwaterman99@gmail.com>
2022-02-17 18:06:54 +0000

    Add docs

--- artefacts/conftest.py
+++ artefacts/conftest.py
@@ -1,3 +1,7 @@
+# This confest.py file is used for configuring the doctests in the package. It
+# needs to be contained in the base of the package's directory for pytest to
+# to correctly create the fixtures used in the doctests.
+
 import pytest
 
 
