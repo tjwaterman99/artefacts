487bc767781c50833f09fea0b756b6db11f38bf6
Tom Waterman <tjwaterman99@gmail.com>
2022-02-20 12:01:17 +0000

    Add linters and formatters

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,21 +2,23 @@ import os
 import pytest
 
 from artefacts.deserializers import Manifest, RunResults, Catalog, Sources
-import artefacts
 
 
-testing_poffertjes_shop = os.environ['DBT_PROJECT_DIR'] == "dbt_projects/poffertjes_shop"
+testing_poffertjes_shop = (
+    os.environ["DBT_PROJECT_DIR"] == "dbt_projects/poffertjes_shop"
+)
 
 
-# I think this should be named something like `deserializer_classes` because 
+# I think this should be named something like `deserializer_classes` because
 # the term "base model" already has a meaning in dbt projects
 def iter_base_models():
     import artefacts.models
+
     for name in dir(artefacts.models):
         obj = getattr(artefacts.models, name)
         try:
             if (
-                issubclass(obj, artefacts.models.Deserializer) \
+                issubclass(obj, artefacts.models.Deserializer)
                 and obj != artefacts.models.Deserializer
             ):
                 yield obj
@@ -26,52 +28,53 @@ def iter_base_models():
 
 def iter_node_reader_classes():
     import artefacts.mixins
+
     for klass in iter_base_models():
         if issubclass(klass, artefacts.mixins.ArtifactNodeReader):
             yield klass
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def reference_docs():
-    with open('docs/reference.rst', 'r') as fh:
+    with open("docs/reference.rst", "r") as fh:
         return fh.read()
 
 
-@pytest.fixture(scope='session', params=list(iter_node_reader_classes()))
+@pytest.fixture(scope="session", params=list(iter_node_reader_classes()))
 def node_reader_class(request):
     yield request.param
 
 
-@pytest.fixture(scope='session', params=list(iter_base_models()))
+@pytest.fixture(scope="session", params=list(iter_base_models()))
 def base_model(request):
     yield request.param
 
 
 @pytest.fixture
 def dbt_project_dir():
-    return os.environ['DBT_PROJECT_DIR']
+    return os.environ["DBT_PROJECT_DIR"]
 
 
-@pytest.fixture(scope='session', params=[Manifest, RunResults, Catalog, Sources])
+@pytest.fixture(scope="session", params=[Manifest, RunResults, Catalog, Sources])
 def all_artifacts(request):
     yield request.param()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def manifest():
     return Manifest()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def catalog():
     return Catalog()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def run_results():
     return RunResults()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def sources():
     return Sources()
